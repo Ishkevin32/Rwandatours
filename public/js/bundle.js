2d4c7107db2da96434071c2f3c290bb0fed99301
Ishkevin32 <ishimwekevin32@gmail.com>
2023-12-08 00:16:15 +0200

    Some files/changes that didn't get committed

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12236,7 +12236,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59734" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50590" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
